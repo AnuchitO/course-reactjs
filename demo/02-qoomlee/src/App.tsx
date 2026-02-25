2363f4019f29d7ee049c06f1a4603d86161372cd
AnuchitO <3372362+AnuchitO@users.noreply.github.com>
2026-02-25 10:40:00 +0700

    demo: complete css style with plain react in single file

--- demo/02-qoomlee/src/App.tsx
+++ demo/02-qoomlee/src/App.tsx
@@ -1,4 +1,5 @@
-import CheckInPage from './components/CheckInPage'
+// import CheckInPage from './components/CheckInPage'
+import CheckInPage from './demo/CheckInDemo'
 
 function App() {
   return <CheckInPage />
